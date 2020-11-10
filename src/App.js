23b063fe5a7588d7adf526e7ec348deb8908c01c
dodichakill <dodiherlangga91@gmail.com>
2020-11-10 05:31:12 -0800

    berhasil mengerjakan eps3. dilocalhost3001

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+import Homepage from "./scenes/Homepage.js";
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Homepage />
     </div>
   );
 }
---
+++ src/scenes/Homepage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Btn from "elements/Button";
+export default function Homepage() {
+  return (
+    <div>
+      <Btn>tes</Btn>
+    </div>
+  );
+}
